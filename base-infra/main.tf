e5d5d36d995ee9efb43ca7d3680d510f0986656d
Sandeep Parikh <parikhs@google.com>
2021-12-01 11:06:36 -0500

    base tf to spin up gke

---
+++ base-infra/main.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.77.0"
+    }
+  }
+}
+
+data "google_project" "project" {
+  project_id = var.project_id
+}
+
+data "google_client_config" "default" {}
+
+provider "google" {
+  region  = var.region
+  project = var.project_id
+}
+
+provider "kubernetes" {
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
+
+module "project_services" {
+  source                      = "terraform-google-modules/project-factory/google//modules/project_services"
+  project_id                  = var.project_id
+  disable_services_on_destroy = false
+  activate_apis = [
+    "compute.googleapis.com",
+    "iam.googleapis.com",
+    "container.googleapis.com"
+  ]
+}
+
+module "gke" {
+  source            = "terraform-google-modules/kubernetes-engine/google"
+  project_id        = var.project_id
+  name              = "${var.deployment_name}-cluster"
+  regional          = true
+  region            = var.region
+  zones             = [var.zone1, var.zone2, var.zone3]
+  network           = "default"
+  subnetwork        = "default"
+  ip_range_pods     = ""
+  ip_range_services = ""
+
+  node_pools = [
+    {
+      name         = "${var.deployment_name}-node-pool"
+      node_count   = 2
+      machine_type = "e2-standard-4"
+      min_count    = 1
+      max_count    = 100
+    }
+  ]
+
+  node_pools_oauth_scopes = {
+    all = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+
+  depends_on = [
+    module.project_services
+  ]
+}
---
+++ base-infra/variables.tf
@@ -0,0 +1,23 @@
+variable "project_id" {
+  description = "GCP project ID"
+}
+
+variable "region" {
+  description = "GCP region"
+}
+
+variable "zone1" {
+  description = "GCP zone 1"
+}
+
+variable "zone2" {
+  description = "GCP zone 2"
+}
+
+variable "zone3" {
+  description = "GCP zone 3"
+}
+
+variable "deployment_name" {
+  description = "Root name for all related resources (cluster, networks, etc)"
+}
